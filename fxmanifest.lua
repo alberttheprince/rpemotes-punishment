2aebf83c0ae9dc89509de0d0249717b0974a3c98
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-07-26 19:30:39 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Demi/Sleepless Development + Popcorn Roleplay'
+description 'Trip players using abusable walk styles!'
+
+version '1.0.0'
+
+---- Uncomment below if you want to use ox_lib ----
+-- shared_scripts { 
+-- 	'@ox_lib/init.lua',
+-- }
+
+client_scripts {
+	'client.lua',
+}
+
+lua54 'yes'
